ff9da2c324425a2e7ab89713bc2eb5b20c496bb9
jolfr <thomas.jack.carroll@gmail.com>
2018-11-13 15:07:27 -0500

    defined custom user model

--- users/models.py
+++ users/models.py
@@ -7,4 +7,17 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    """User information"""
+    user_token = models.CharField(max_length=1024)
+    email = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+    picture = models.TextField
+    given_name = models.CharField(max_length=256)
+    family_name = models.CharField(max_length=256)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.email
+
