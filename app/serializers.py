6ef8a6b697216366329dea1133243b630ed5f6a8
jolfr <thomas.jack.carroll@gmail.com>
2018-11-13 14:25:48 -0500

    created users app

--- app/serializers.py
+++ app/serializers.py
@@ -1,18 +1,12 @@
-from django.contrib.auth.models import User, Group
 from app.models import Topic, Entry
 from rest_framework import serializers
+from .models import User
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
 
 
 class TopicSerializer(serializers.HyperlinkedModelSerializer):
--- backend/urls.py
+++ backend/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from rest_framework import routers
 from app import views
-from app.views import login
+from users.views import login
 
 
 router = routers.DefaultRouter()
